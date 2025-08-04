0a1b5effc89b5e0265c17439240ba090fbfe5666
i.hrek <illiagreek@gmail.com>
2025-08-05 00:50:15 +0300

    па

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'picsum.photos', 'beautypapers.com'],
+    unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 }
 
 module.exports = nextConfig 
